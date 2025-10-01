a9394c607733f8960ee109b95d5d4d5bf7b37fc7
Vandan Dalvi <vandandalvi@gmail.com>
2025-10-02 02:02:40 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Vandan Dalvi</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
+        <h5 className="text-light">Full Stack Developer + AIML</h5>
         <CTA />
         
       </div>
