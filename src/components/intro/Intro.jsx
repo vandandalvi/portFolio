9e84eea044df76b003978a1baf7638e14da10791
Vandan Dalvi <vandandalvi@gmail.com>
2025-10-02 01:11:19 +0530

    Update Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,7 @@ import "./intro.css";
 import { FaAward } from "react-icons/fa";
 import React from "react";
 import { VscFolderLibrary } from "react-icons/vsc";
-import img from '../../assets/me.jpg'
+import img from '../../assets/me2.jpg'
 
 
 const Intro = () => {
