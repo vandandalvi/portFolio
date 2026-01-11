aaad4035782a84e292e39721647090297380808c
Vandan Dalvi <vandandalvi@gmail.com>
2026-01-11 14:33:37 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/vandan-dalvi/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://www.linkedin.com/in/vandandalvi/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
       <a href="https://github.com/vandandalvi" target="_blank" rel="noreferrer" ><FaGithub /></a>
       </div>
       <div className="footer__copyright">
